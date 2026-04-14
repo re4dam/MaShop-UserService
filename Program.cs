2fdfacafdacd57c8698497f741f58eef6c72afbb
Readam <zakiadam332@gmail.com>
2026-04-14 15:13:32 +0700

    Add UsersController and BCrypt hasher
    Introduce UsersController with CRUD endpoints and a register action that hashes passwords. Add IPasswordHasher interface and BCryptPasswordHasher implementation (BCrypt.Net). Register the password hasher in Program.cs and add BCrypt.Net-Next package to the project file. Changes wire up user persistence via UserDbContext and use DTOs for request/response handling.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using UserService.Data;
+using UserService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,9 @@ builder.Services.AddEndpointsApiExplorer();
 // Register DbContext (Using SQL Server)
 builder.Services.AddDbContext<UserDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Register Services
+builder.Services.AddScoped<IPasswordHasher, BCryptPasswordHasher>();
 
 var app = builder.Build();
 
